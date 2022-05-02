1a6fe98a87a37c0f2d0ab7ad997062d1399baa33
Celine LEIBENGUTH <cleibeng@e1r5p4.clusters.42paris.fr>
2022-05-02 20:31:31 +0200

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cleibeng <cleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:22:38 by cleibeng          #+#    #+#             */
-/*   Updated: 2022/05/02 17:30:19 by cleibeng         ###   ########.fr       */
+/*   Updated: 2022/05/02 20:31:15 by cleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,26 +69,28 @@ char	*get_next_line(int fd)
 {
 	static char	*buf_static;
 	char		*bufread;
+	int	i;
 
 	if (fd < 0 || fd > 1023 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!buf_static)
 		buf_static = NULL;
 	else if (ft_buf_read(buf_static) > 0)
-		return (ft_return(buf_static));
+		return (ft_return(buf_static)); // ok jusque la
 	bufread = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!bufread)
 		return (NULL);
-	while (read(fd, bufread, BUFFER_SIZE) > 0)
+	while (i > 1 && ft_buf_read(bufread) != )
 	{
-		printf("ok");
-		bufread[BUFFER_SIZE + 1] = '\0';
-		buf_static = ft_strjoin(buf_static, bufread);
-	}
+		printf("%s\n", bufread);
+		i = read(fd, bufread, BUFFER_SIZE);
+//		buf_static = ft_strjoin(buf_static, bufread);
+//		printf("%s\n", buf_static);
+	}/*
 	ft_clean(bufread);
 	if (buf_static != NULL)
 		return (ft_return(buf_static));
-	return (NULL);
+	return (NULL);*/
 }
 
 int	main(void)
