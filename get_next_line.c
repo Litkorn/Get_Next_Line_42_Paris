97d06cf3923b703f44a608f0d9a06fcdfa757e70
Celine LEIBENGUTH <cleibeng@e1r3p6.42.fr>
2022-05-02 17:31:50 +0200

    wip, buf_read ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,102 +6,102 @@
 /*   By: cleibeng <cleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:22:38 by cleibeng          #+#    #+#             */
-/*   Updated: 2022/04/30 17:49:28 by cleibeng         ###   ########.fr       */
+/*   Updated: 2022/05/02 17:30:19 by cleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_split_end(char **buf, char **buf_gnl, int n)
+static int	ft_split_end(char **buf, char **buf_static, int n)
 {
 	int		i;
-	char	*str;
 
 	i = 0;
-	if (!(str = malloc(sizeof(char) * (n + 1)));
+	buf = malloc(sizeof(char) * (n + 1));
+	if (!buf)
 		return (0);
-	while (*buf[i] && *buf_gnl[i] && i < n)
+	while (*buf[i] && *buf_static[i] && i < n)
 	{
-		*buf[i] = *buf_gnl[i];
+		*buf[i] = *buf_static[i];
 		i++;
 	}
 	*buf[i] = '\0';
-	*buf_gnl = ft_strdup(buf_gnl[i]); // <-  a verifier
-	return (1);
-}
-
-char	*ft_return(char *buf_gnl)
-{
-	char	*buf;
-	int		i;
-
-	i = 0;
-	if (ft_buf_read(buf_gnl) == 0)
-		return(NULL);
-	if (ft_buf_read(buf_gnl) == 1)
-	{
-		while (buf_gnl[i] != '\n')
-			i++;
-		if (buf_gnl[i + 1] != '\0')
-			ft_split_end(&buf, &buf_gnl, (i));
-		return (buf);
-	}
-// a completer pour == 2
-}
-int	ft_split_end(char **buf, char **buf_gnl, int n)
-{
-	int		i;
-	char	*str;
-
-	i = 0;
-	if (!(str = malloc(sizeof(char) * (n + 1)));
-		return (0);
-	while (*buf[i] && *buf_gnl[i] && i < n)
-	{
-		*buf[i] = *buf_gnl[i];
-		i++;
-	}
-	*buf[i] = '\0';
-	*buf_gnl = ft_strdup(buf_gnl[i]); // <-  a verifier
+	*buf_static = ft_strdup(buf_static[i]);
 	return (1);
 }
 
 /* lit le buffer: si un \n -> renvoie 1, si pas de buf-> renvoie 0,
 si pas de \n mais buf != \0 -> renvoie 2 */
-int	ft_read_buf(char *buf)
+static int	ft_buf_read(char *buf)
 {
 	int	i;
 
 	i = 0;
-	while (buf[i] || buf[i] != '\n')
+	while (buf[i] != '\0' && buf[i] != '\n')
 		i++;
 	if (buf[i] == '\n')
 		return (1);
-	if (!buf)
-		return (0);
 	return (2);
+}
+
+static char	*ft_return(char *buf_static)
+{
+	char	*buf;
+	int		i;
+
+	i = 0;
+	buf = NULL;
+	if (ft_buf_read(buf_static) == 0)
+		return (NULL);
+	if (ft_buf_read(buf_static) == 1)
+	{
+		while (buf_static[i] != '\n')
+			i++;
+		if (buf_static[i + 1] != '\0')
+			ft_split_end(&buf, &buf_static, (i));
+		return (buf);
+	}
+	buf = ft_strdup(buf_static);
+	return (ft_clean(buf_static), buf);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buf_gnl;
+	static char	*buf_static;
 	char		*bufread;
 
 	if (fd < 0 || fd > 1023 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!buf_gnl)
-		buf_gnl = NULL;
-	else if (ft_read_buf(buf_gnl) > 0)
-		return (ft_return(buf_gnl));
-	if (!(bufread = malloc(sizeof(char) * (BUFFER_SIZE + 1))
+	if (!buf_static)
+		buf_static = NULL;
+	else if (ft_buf_read(buf_static) > 0)
+		return (ft_return(buf_static));
+	bufread = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!bufread)
 		return (NULL);
 	while (read(fd, bufread, BUFFER_SIZE) > 0)
 	{
+		printf("ok");
 		bufread[BUFFER_SIZE + 1] = '\0';
-		buf_gnl = ft_strjoin(buf_gnl, bufread);
+		buf_static = ft_strjoin(buf_static, bufread);
 	}
 	ft_clean(bufread);
-	if (buf_gnl != NULL)
-		return (ft_return(buf_gnl));
+	if (buf_static != NULL)
+		return (ft_return(buf_static));
 	return (NULL);
+}
+
+int	main(void)
+{
+	int	fd = open("test.txt", O_RDONLY);
+	char *str;
+
+	str = get_next_line(fd);
+	while (str != NULL)
+	{
+		printf("%s", str);
+		str = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,30 @@
 /*   By: cleibeng <cleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:22:43 by cleibeng          #+#    #+#             */
-/*   Updated: 2022/04/30 14:42:05 by cleibeng         ###   ########.fr       */
+/*   Updated: 2022/05/02 15:42:12 by cleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *str1, const char *str2)
+void	ft_clean(char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *str1, const char *str2);
 size_t	ft_strlen(const char *s);
-
 
 #endif
